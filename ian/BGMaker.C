e7d5818134f9df038f7c0e03abae15838aca3491
Ian <iwessen@umail.ucsb.edu>
2013-08-08 07:56:34 -0700

    file deletion

--- ian/BGMaker.C
+++
@@ -1,5 +0,0 @@
-void MakeBackground(){
-  RooDataSet Background_Combined("Background_Combined");
-  for(std::vector<TString>::iterator lblit = lblvec.begin(); lblit != lblvec.end(); ++lblit){
-    Background_Combined.append(*lblit + "_Combined")
-  }
