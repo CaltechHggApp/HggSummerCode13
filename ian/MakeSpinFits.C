838a27c71f45f24b1b3267808a3b8c083fd5e2f8
Ian <iwessen@umail.ucsb.edu>
2013-08-12 09:57:42 -0700

    Succesful compilation and run; MakeBackground method

--- ian/MakeSpinFits.C
+++ ian/MakeSpinFits.C
@@ -855,7 +855,7 @@ RooAbsPdf* MakeSpinFits::Make2DBkgModel(TString massMcName,TString costMcName,TS
 void MakeSpinFits::MakeBackground(){
   RooDataSet Background_Combined(*(ws->data("Data_Combined")),"Background");
   for (auto mcIt=mcLabel.begin(); mcIt != mcLabel.end(); mcIt++){
-    append(*(ws->data(*mcIt+"_Combined")),*mcIt);
+    Background_Combined.append(*(ws->data(*mcIt+"_Combined")));
   }
 }
 
